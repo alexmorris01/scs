2108877ed87ad87f0851113363a70f920c027d40
alexmorris01 <66241079+alexmorris01@users.noreply.github.com>
2020-06-01 08:56:26 +0100

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+public GameObject[] objectospawn;
+public  int max = 12;
+public  int min = 12;
+public int minusnumber = 1;
+public int count;
+public void NewSpawn()
+{
+//makes the random not random at all. this will make the array spawn each object one after the other, when requested
+min = min  -  minusnumber;
+max = max -  minusnumber;
+print("new email");
+            GameObject obj1 = Instantiate(objectospawn[Random.Range(min, max)]) as GameObject;
+            obj1.transform.parent = gameObject.transform;
+         }
+     }
---
+++ StarSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starsystem : MonoBehaviour
+{
+public GameObject OriginalObject;
+public GameObject one1star;
+public GameObject two2star;
+public GameObject three3star;
+public int minmistake1;
+public int maxmistake1;
+public int minmistake2;
+public int maxmistake2;
+public int minmistake3;
+public int maxmistake3;
+    void Update()
+    {
+if(OriginalObject.gameObject.GetComponent<MistakesCounter>().nonmistakes >= minmistake1 && OriginalObject.gameObject.GetComponent<MistakesCounter>().nonmistakes  <= maxmistake1)
+{
+onestar();
+}
+if(OriginalObject.gameObject.GetComponent<MistakesCounter>().nonmistakes >= minmistake2 && OriginalObject.gameObject.GetComponent<MistakesCounter>().nonmistakes <= maxmistake2)
+{
+twostar();
+}
+if(OriginalObject.gameObject.GetComponent<MistakesCounter>().nonmistakes >= maxmistake3)
+{
+threestar();
+}
+}
+void onestar()
+{
+print("onestar");
+one1star.SetActive(true);
+}
+void twostar()
+{
+print("twostar");
+two2star.SetActive(true);
+one1star.SetActive(false);
+}
+void threestar()
+{
+print("threestar");
+three3star.SetActive(true);
+two2star.SetActive(false);
+one1star.SetActive(false);
+}     
+}
